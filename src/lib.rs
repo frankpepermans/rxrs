c9d29fe99cbbb332793131e8aa6dee7ed53e9bef
frankpepermans <frank@igindo.com>
2025-01-01 01:05:44 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,21 @@ pub mod stream;
 pub mod subject;
 
 pub mod prelude {
+    pub use crate::{
+        stream::event::*,
+        stream::rx::combine_latest::*,
+        subject::{behavior_subject::*, publish_subject::*, replay_subject::*},
+    };
+}
+
+pub mod streams {
+    pub use crate::stream::rx::combine_latest::*;
+}
+
+pub mod subjects {
+    pub use crate::subject::{behavior_subject::*, publish_subject::*, replay_subject::*, *};
+}
+
+pub mod events {
     pub use crate::stream::event::*;
-    pub use crate::stream::*;
-    pub use crate::subject::behavior_subject::*;
-    pub use crate::subject::publish_subject::*;
-    pub use crate::subject::replay_subject::*;
-    pub use crate::subject::*;
 }
