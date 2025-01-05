ccdc36e540c6254b69fc9ded1bbb46f5f0aba898
frankpepermans <frank@igindo.com>
2025-01-05 20:48:52 +0100

    actually fix debounce

--- src/stream_ext/debounce.rs
+++ src/stream_ext/debounce.rs
@@ -6,11 +6,11 @@ use std::{
 };
 
 use futures::{
-    future::{select, Either, FusedFuture},
+    future::{select, Either},
     stream::{Fuse, FusedStream},
     FutureExt, Stream, StreamExt,
 };
-use futures_timer::Delay;
+use futures_time::future::FutureExt as OtherFutureExt;
 use pin_project_lite::pin_project;
 
 pin_project! {
@@ -21,7 +21,7 @@ pin_project! {
         stream: Fuse<S>,
         duration: Duration,
         #[pin]
-        current_interval: Option<Delay>,
+        current_interval: Option<Pin<Box<dyn Future<Output = bool>>>>,
         candidate_event: Option<S::Item>,
     }
 }
@@ -51,56 +51,64 @@ impl<S: Stream> Stream for Debounce<S> {
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
+        let duration = this.duration.as_millis().try_into().unwrap();
+
+        fn create_interval(
+            existing: bool,
+            duration: u64,
+        ) -> Option<Pin<Box<dyn Future<Output = bool>>>> {
+            if existing {
+                let deadline = futures_time::time::Duration::from_millis(duration);
+
+                Some(Box::pin(async { true }.delay(deadline)))
+            } else {
+                None
+            }
+        }
 
         if let Some(interval) = this.current_interval.as_mut().as_pin_mut() {
-            let mut switch = select(interval, this.stream.next());
-
-            match switch.poll_unpin(cx) {
+            match select(interval, this.stream.next()).poll_unpin(cx) {
                 Poll::Ready(it) => match it {
-                    Either::Left((_, other)) => {
-                        if other.is_terminated() {
-                            this.current_interval.set(None);
-                            return Poll::Ready(this.candidate_event.take());
-                        }
+                    Either::Left(_) => {
+                        *this.current_interval = create_interval(false, duration);
+
+                        Poll::Ready(this.candidate_event.take())
                     }
-                    Either::Right((it, other)) => match it {
-                        Some(item) => match other.poll(cx) {
-                            Poll::Ready(_) => {
-                                this.current_interval.set(None);
-                                return Poll::Ready(this.candidate_event.take());
-                            }
-                            Poll::Pending => {
-                                this.current_interval.set(Some(Delay::new(*this.duration)));
-                                *this.candidate_event = Some(item);
-                            }
-                        },
-                        None => return Poll::Ready(this.candidate_event.take()),
+                    Either::Right((it, mut interval)) => match it {
+                        Some(item) => {
+                            interval.set(create_interval(true, duration).unwrap());
+                            *this.candidate_event = Some(item);
+
+                            cx.waker().wake_by_ref();
+
+                            Poll::Pending
+                        }
+                        None => Poll::Ready(this.candidate_event.take()),
                     },
                 },
-                Poll::Pending => return Poll::Pending,
+                Poll::Pending => Poll::Pending,
             }
         } else {
             match this.stream.poll_next(cx) {
                 Poll::Ready(Some(item)) => {
-                    this.current_interval.set(Some(Delay::new(*this.duration)));
+                    *this.current_interval = create_interval(true, duration);
                     *this.candidate_event = Some(item);
+
+                    cx.waker().wake_by_ref();
+
+                    Poll::Pending
                 }
-                Poll::Ready(None) => return Poll::Ready(this.candidate_event.take()),
-                Poll::Pending => return Poll::Pending,
-            };
-        };
-
-        cx.waker().wake_by_ref();
-
-        Poll::Pending
+                Poll::Ready(None) => Poll::Ready(this.candidate_event.take()),
+                Poll::Pending => Poll::Pending,
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::{thread, time::Duration};
-
     use futures::{executor::block_on, stream, Stream, StreamExt};
+    use futures_time::{future::FutureExt, time::Duration};
 
     use crate::RxExt;
 
@@ -110,7 +118,7 @@ mod test {
 
         block_on(async {
             let all_events = stream
-                .debounce(Duration::from_millis(100))
+                .debounce(std::time::Duration::from_millis(100))
                 .collect::<Vec<_>>()
                 .await;
 
@@ -126,7 +134,7 @@ mod test {
                     _ => Duration::from_millis(50),
                 };
 
-                thread::sleep(interval);
+                async { true }.delay(interval).await;
 
                 Some((count, count + 1))
             } else {
